3d2cfccae97e5d68c8fc172397c07fe41170caff
Luke Segars <luke@lukesegars.com>
2017-06-01 10:34:58 -0400

    Minor changes (ng2 => ng4?) to make this compile

--- app/app.boot.ts
+++ app/app.boot.ts
@@ -2,3 +2,9 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.modules';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+
+// import { platformBrowser }    from '@angular/platform-browser';
+// import { AppModuleNgFactory } from '../out/app/app.modules.ngfactory';
+
+// console.log('Running AOT compiled');
+// platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
--- app/component.artist-item.ts
+++ app/component.artist-item.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Artist } from './component.app';
 
 @Component({
   selector: 'artist-item',
-  templateUrl: 'partials/artist-item.html',
-  styleUrls: ['css/artist-item.css'],
-  inputs: ['artist']
+  templateUrl: '../partials/artist-item.html',
+  styleUrls: ['../css/artist-item.css'],
 })
 
-export class ArtistItemComponent {}
+export class ArtistItemComponent {
+  @Input() artist: Artist = null;
+} 
